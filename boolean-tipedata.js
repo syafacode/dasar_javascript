c1e4a49eee8fa17da86997e73b2e3e422d2b5096
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-04-27 23:39:55 +0700

    boolean

---
+++ boolean-tipedata.js
@@ -0,0 +1,20 @@
+//cara pendefinisian nilai boolean
+var tinggi = false;
+
+//penulisan kurawal
+var cm = 165;
+
+if(cm>170){
+    tinggi = true;
+}else{
+    tinggi = false;
+}
+
+//penulisan langsung
+if(cm>170)
+tinggi = true
+else
+tinggi = false
+
+//eksekusi
+console.log(tinggi);
