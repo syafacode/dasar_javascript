c5ad50a32e8f37d56b369d052a3419069c7a212b
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-04-27 23:40:11 +0700

    string

---
+++ string-tipedata.js
@@ -0,0 +1,14 @@
+//digunakan untuk dua kata/dg spasi, misal
+var kata = "Selamat pagi!"
+var huruf = "abcdefghijklmnopqrstuvwxyz"
+//bila menggunakan petik satu juga sama saja
+var tunggal = 'tunggal'
+var hari = "Jum'at"
+//bila ingin memberi tanda petik sebagai percakapan 
+var ibu ='"Pergi!" kata ibu'
+
+console.log(kata);
+console.log(huruf);
+console.log(tunggal);
+console.log(hari);
+console.log(ibu);
