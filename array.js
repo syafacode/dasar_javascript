a7a52eb543f47b82c044a8f04d1f4d1524da682a
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-04-27 23:40:32 +0700

    array

---
+++ array.js
@@ -0,0 +1,8 @@
+var angka = [3,4,5,6,7];
+
+//menampilkan semua isi array
+console.log(angka);
+//melihat data array dalam index 1 yaitu angka 4
+console.log(angka[1]);
+//melihat jumlah data pada array
+console.log(angka.length);
