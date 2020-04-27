d537fab7715069967760c9816304e055cafcbaa9
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-04-27 23:39:39 +0700

    without define variable

---
+++ variable-none.js
@@ -0,0 +1,23 @@
+let msg1 = "Hallo";
+const msg2 = "coding"
+
+//pada dasarnya bersifat var
+//kita harus mengisi nilainya harus bertipe-data
+//agar bisa tanpa mendefinisikan variable
+//contoh
+msg3 = "Berhasil";
+aku = 1;
+adalah = null;
+anak = false;
+rajin = 1.0;
+
+if(true){
+    let msg1 = "Yes";
+    var message = "ini message";
+
+    //karena bersifat variable maka akan berubah nilai dari var
+    msg3 = "Selamat"
+}
+
+//output akan selamat
+console.log(msg3);
