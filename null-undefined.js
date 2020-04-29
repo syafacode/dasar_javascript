15222b429e007652d6102eb765331eba34679e91
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-04-30 00:08:06 +0700

    null and undefined variable

---
+++ null-undefined.js
@@ -0,0 +1,9 @@
+//nilai undefined terjadi apabila belum diisi nilai variablenya
+var undef;
+
+//nilai null adalah nilai yang digunakan untuk data sementara dikosongi terlebih dahulu
+var kosong = null;
+
+console.log(undef);
+console.log(kosong);
+
