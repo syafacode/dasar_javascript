1aef0f821a298bf6f433d883e8f55a39d3c55ae8
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-04-30 00:08:57 +0700

    if conditional

---
+++ if-else.js
@@ -0,0 +1,13 @@
+function angka(){
+    var nilai = 1;
+
+    if(nilai === 3){
+        console.log("Nilai adalah 3");
+    }else if(nilai ===2){
+        console.log("Nilai adalah 2");
+    }else{
+        console.log("Nilai adalah "+ nilai);
+    }
+}
+
+angka();
