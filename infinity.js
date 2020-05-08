168bab1c8f2134364f651f24f663b655c7dbc2f4
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-05-08 08:35:30 +0700

    infinity looping

---
+++ infinity.js
@@ -0,0 +1,17 @@
+//using for
+function infFor() {
+    for (var i = 0; i >= 0; i++) {
+        console.log(i);
+    }
+}
+
+//using while
+function infWhile() {
+    var x = 1
+    while (true) {
+        console.log(x);
+        x++;
+    }
+}
+
+infWhile();
