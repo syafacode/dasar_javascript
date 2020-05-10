c5b5efb7bc66b740c27ed82864ff05e16b55ba6e
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-05-10 13:03:05 +0700

    switch

---
+++ switch.js
@@ -0,0 +1,16 @@
+function switchCase(){
+    var nilai = 60;
+
+    switch(nilai){
+        case 100:
+            console.log("Sangat Baik Sekali");
+            break;
+        case 75:
+            console.log("Nilai KKM");
+            break;
+        default:
+            console.log("Remidi")
+    }
+}
+
+switchCase();
