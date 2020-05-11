c903da08f10190c2e0c802b42059f75f8a457e77
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-05-11 17:34:51 +0700

    create function

---
+++ function.js
@@ -0,0 +1,18 @@
+//penulisan fucntion cara lama
+function jalanSatu(){
+    console.log("Jalan Satu");
+}
+
+function jalanDua(){
+    console.log("Jalan Dua")
+}
+
+jalanSatu();
+jalanDua();
+
+//penulisan function cara baru
+const jalanTiga = ()=>{
+    console.log("Jalan Tiga");
+}
+
+jalanTiga();
