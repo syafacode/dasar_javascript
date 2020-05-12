5ad5ba32e51dacd3f87d4db66f4fedf5c86b561e
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-05-12 13:45:15 +0700

    for of

---
+++ for-of.js
@@ -0,0 +1,8 @@
+var angka = [3,0,9,2,4,5,1,5];
+
+for(let x of angka){
+    console.log(x);
+}
+
+//ket: x merupakan variabel untuk data menampilkan isi array, 
+//kita tidak tidak perlu menuliskan index dari array angka, GUNAKAN FOR OF
