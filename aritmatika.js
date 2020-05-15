197cb42b035cb4a766991c74969e5aa20c49a981
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-05-15 12:12:21 +0700

    aritmatika

---
+++ aritmatika.js
@@ -0,0 +1,8 @@
+var a = 10;
+var b = 20;
+
+console.log(a + b);//tambah
+console.log(a - b);//kurang
+console.log(a * b);//dikali
+console.log(a / b);//bagi
+console.log(a % b);//modular (sisa bagi)
