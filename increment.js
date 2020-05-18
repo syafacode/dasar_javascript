6bcf18bc1f046f9a3c0534d7c82a61bee7b862ff
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-05-18 14:27:14 +0700

    increment and decrement

---
+++ increment.js
@@ -0,0 +1,24 @@
+//increment using ++
+function increment() {
+    var x = 0;
+    console.log(x);
+    x++;
+    console.log(x);
+    x++;
+    console.log(x);
+}
+
+//decrement using --
+function decrement(){
+    var x = 10;
+    console.log(x);
+    x--;
+    console.log(x);
+    x--;
+    console.log(x);
+}
+
+console.log("Increment");
+increment();
+console.log("Decrement");
+decrement();
