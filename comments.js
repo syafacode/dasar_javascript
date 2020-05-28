e151c3df70efbfc7d86818ad7b2ef707dfd4375e
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-05-28 08:25:31 +0700

    how to comment

---
+++ comments.js
@@ -0,0 +1,10 @@
+// comment menggunakan garis miring 2 kali
+function caraComments() {
+    console.log('pesan satu');
+    console.log('pesan dua');
+    //console.log('pesan tiga'); tidak tserbaca satu line
+    /* console.log('pesan satu'); tidak terbaca 2 line
+    console.log('pesan dua'); */
+}
+
+caraComments();
