3bf1390dfd599edc9cbc80897f8376b4e78507e4
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-05-30 14:16:22 +0700

    return

---
+++ return.js
@@ -0,0 +1,20 @@
+function nilai(){
+    //mengembalikan nilai string
+    return "Nilai 100"
+}
+console.log(nilai());
+
+function angka(){
+    var bil = 100;
+    return bil;
+}
+console.log(angka());
+
+function ambil(){
+    var lempar = "diterima";
+    //mengembalikan nilai yang diterima ke layar
+    return lempar;
+}
+//menyimpan nilai function dalam var
+var tampung = ambil();
+console.log(tampung);
