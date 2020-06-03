7ac43a4a738bd3d7e883619de5afb61909545756
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-06-03 20:53:03 +0700

    nested if

---
+++ nested-if.js
@@ -0,0 +1,17 @@
+var x = 25;
+var y = 30;
+var z = 10;
+
+if(x>y){
+    if(x>z){
+        console.log("Nilai X adalah yang paling besar");
+    }else {
+        console.log("Nilai X adalah yang terbesar ke 2");
+    }
+} else {
+    if(x<z){
+        console.log("Nilai y terbesar");
+    }else{
+        console.log("Nilai y terbesar ke 2");
+    }
+}
