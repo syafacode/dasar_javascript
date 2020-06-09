3cc48dd35042eca022ce9d4a221022d0d9180b28
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-06-09 22:34:08 +0700

    operator

---
+++ perbandingan.js
@@ -0,0 +1,11 @@
+var x = 10;
+var z = 11;
+
+console.log(x > z);//lebih dari
+console.log(x < z);//kurang dari
+console.log(x >= z);//lebih dari sama dengan
+console.log(x <= z);//kurang dari sama dengan
+console.log(x == z);//sama dengan
+console.log(x === z);//sama dengan typedata
+console.log(x != z);//tidak sama dengan
+console.log(x !== z);//tidak sama dengan typedata
