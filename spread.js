a4dc49a686b7bbb196f31639d82a82e1b8e1c2ae
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-06-13 00:35:51 +0700

    spread
    digunakan untuk menuliskan value index pada sebuah array

---
+++ spread.js
@@ -0,0 +1,11 @@
+//digunakan untuk menggabungkan array
+var angka1 = [1,2,3,4];
+
+var angka2 = [5,6,7,8];
+
+//menggunakan 3 titik diikuti nama array
+var gabungan = [...angka1,...angka2,9,10,11,12];
+
+console.log(angka1);
+console.log(angka2);
+console.log(gabungan);
