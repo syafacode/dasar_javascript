c7947b55ee76de76e2c76c2e6bb5f038081952dc
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-07-13 21:37:19 +0700

    callback

---
+++ callback.js
@@ -0,0 +1,12 @@
+const getCake = callback => {
+    let cake = null;
+    console.log("Sedang membuat kue, silakan tunggu ....")
+    setTimeout(() => {
+        cake = "Kue Selesai!";
+        callback(cake);
+    }, 3000);
+}
+
+getCake(cake => {
+    console.log(cake);
+})
